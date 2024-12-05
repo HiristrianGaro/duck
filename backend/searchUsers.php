45754ef1163322957522efd95b3ee24f164044c2
Christian Gallo <christiangallo20@gmail.com>
2024-12-05 14:36:05 +0100

    finding users for search

---
+++ backend/searchUsers.php
@@ -0,0 +1,16 @@
+<?php
+// Avvia la sessione
+if(session_status() !== PHP_SESSION_ACTIVE) session_start();
+require_once("../common/connection.php");
+$emailUtente = $_SESSION['Username'];
+
+// Controlla la connessione
+if ($cid->connect_error) {
+    die("Connessione fallita: " . $cid->connect_error);
+}
+
+
+
+header('Content-Type: application/json');
+
+$input = json_decode(file_get_contents('php://input'), true);
