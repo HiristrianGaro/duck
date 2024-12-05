7b2e11d879281706d663dd4a5c89aafe89b656b8
Christian Gallo <christiangallo20@gmail.com>
2024-12-05 14:37:07 +0100

    removed usless libraries

--- frontend/loginPage.php
+++ frontend/loginPage.php
@@ -3,6 +3,8 @@
     session_unset();
     require_once("../common/connection.php");
     require_once("../backend/login.php");
+    include("../common/modalErrore.php");
+    include("../common/errorModal.php");
 ?>
 
 <div class="d-flex justify-content-center px-lg-3">
@@ -26,10 +28,3 @@
         <p class="mt-5 mb-3 text-body-secondary">&copy; Duck–2024</p>
     </form>
 </div>
-
-<?php include("../common/modalErrore.php")?>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-
-<?php include("../common/errorModal.php")?>
