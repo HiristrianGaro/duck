6ee04249e86e589506ed1cba778c5f1e8294876b
Christian Gallo <christiangallo20@gmail.com>
2024-12-05 10:34:56 +0100

    cleaned

--- index.php
+++ index.php
@@ -1,15 +1,10 @@
 <?php
-session_start();
-include "config.php";
-include "common/connection.php";
-include "common/funzioni.php";
-include "backend/login.php";
-include "backend/register.php";
-if (isset($_SESSION['Status'])) {
-  error_log('User is logged in');
-} else {
-  error_log('User is NOT logged in');
-}
+  session_start();
+  include "config.php";
+  include "common/connection.php";
+  include "common/funzioni.php";
+  include "backend/login.php";
+  include "backend/register.php";
 ?>
 
 
@@ -23,28 +18,20 @@ if (isset($_SESSION['Status'])) {
 
 
     <?php include "common/navbar.php"; ?>
+    <?php include "common/searchCollapse.php"; ?>
+    <div class="container" id="main-page">
 
-      <div class="container" id="main-page">
-        <div class="collapse" id="collapseExample">
-          <div class="d-flex justify-content-center px-lg-3">
-            <div class="p-3 p-md-4 border rounded-3 bg-white">
-              <form class="form-inline">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" id="searchInput">
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-        <?php if(!isset($_SESSION['Status'])) {include 'frontend/landingPage.html';}?>
+      
+      <?php if(!isset($_SESSION['Status'])) {include 'frontend/landingPage.html';}?>
         
-      </div>
+    </div>
     
   </main>
   <?php include "common/footer.php"; ?>
   <?php include("common/modalErrore.php")?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
   <?php include("common/errorModal.php")?>
 </body>
