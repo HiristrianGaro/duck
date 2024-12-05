1af71cc6acbf911f0358bc208c78dc7fa2146e06
Christian Gallo <christiangallo20@gmail.com>
2024-12-05 10:35:55 +0100

    link changed

--- common/header.php
+++ common/header.php
@@ -8,9 +8,10 @@
   <meta property="og:title" content="Duck.com">
   <meta property="og:type" content="website">
   <meta property="og:description" content="Quack Quack">
+  <link rel="icon" type="image/x-icon" href="../assets/brand/duck.jpg">
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" defer></script>
-  <link href="/Duck/css/main.css" rel="stylesheet">
+  <link href="/Duck/css/style.css" rel="stylesheet">
 </head>
