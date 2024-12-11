dc7b241d723d87f98a2f9b9de45d661191825681
Christian Gallo <christiangallo20@gmail.com>
2024-12-11 19:45:56 +0100

    idk

--- common/postContent.php
+++ common/postContent.php
@@ -1,51 +1,70 @@
-<?php
-    $Autore = $Post['AutorePostEmail'];
-    $dataPubblicazione = $Post['DataDiPubblicazione'];
-    $orario = $Post['OraDiPubblicazione'];
-    $dataOrarioMessaggio = $dataPubblicazione . ' ' . $orario;
-    $timestamp = strtotime($dataOrarioMessaggio);
-    $displayData = date("d F Y, H:i", $timestamp);
-    $postId = $emailAutore.$dataOrarioMessaggio;
-    $postId = preg_replace('/[^a-zA-Z0-9-_]/', '', $postId);
-    require_once ("../Backend/getFoto.php");
-    $foto = getFoto($emailAutore, $dataPubblicazione, $orario, $conn);
-    $luogoFoto ='';
-    if (!empty($foto['Città'])){
-        $luogoFoto = $foto['Stato'].','.$foto['Regione'].','.$foto['Città'];
-    }
 
-?>
-<div class="card gedf-card">
-    <div class="card-header">
-            <div class="d-flex justify-content-between align-items-center">
-                <div>
-                    <img class="rounded-circle" width="45" src="https://picsum.photos/50/50" alt="">
-                </div>
-                <div class="ml-1">
-                    <div class="h5 m-0">@LeeCross</div>
-                    <div class="h7 text-muted">Miracles Lee Cross</div>
-                </div>
-                <div class="text-muted h7 mb-2 ml-auto"> <i class="fa fa-clock-o"></i>10 min ago</div>
-            </div>
-    </div>
-    <div class="container-sm card-body">
-        <div class="d-flex justify-content-center align-items-center mx-1">
-            <img src="./assets/images/post/pfp1.jpg" class="img-fluid w-100 post-image" alt="">
-        </div>
+<div class="row">	
+<div class="col-lg-6">
 
-        <p class="card-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo recusandae nulla rem eos ipsa praesentium esse magnam nemo dolor
-            sequi fuga quia quaerat cum, obcaecati hic, molestias minima iste voluptates.
-        </p>
+<div class="card">
+    
+    <div class="card-heading">
+    <!-- START dropdown-->
+    <div class="dropdown float-right">
+    <button class="btn btn-flat btn-flat-icon" type="button" data-toggle="dropdown" aria-expanded="false">
+    <em class="fa fa-ellipsis-h"></em>
+    </button>
+    <div class="dropdown-menu dropdown-scale dropdown-menu-right" role="menu" style="position: absolute; transform: translate3d(-136px, 28px, 0px); top: 0px; left: 0px; will-change: transform;">
+    <a class="dropdown-item" href="#">Hide post</a>
+    <a class="dropdown-item" href="#">Stop following</a>
+    <a class="dropdown-item" href="#">Report</a>
     </div>
-    <div class="card-footer">
-        <div class="d-flex justify-content-left">
-            <p class="d-inline-flex gap-1">
-                <button class="btn btn-link bi bi-chat" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"></button>
-            </p>
-            <div class="collapse" id="collapseExample">
-                <?php include './common/commenti.php'; ?>
-            </div>
-        </div>
+    </div><!--/ dropdown -->
+    <div class="media m-0">
+    <div class="d-flex mr-3">
+    <a href=""><img class="img-fluid rounded-circle" src="http://www.themashabrand.com/templates/bootsnipp/post/assets/img/users/4.jpg" alt="User"></a>
     </div>
+    <div class="media-body">
+    <p class="m-0">Benjamin Robinson</p>
+    <small><span><i class="icon ion-md-pin"></i> Nairobi, Kenya</span></small>
+    <small><span><i class="icon ion-md-time"></i> 10 hours ago</span></small>
+    </div>
+    </div><!--/ media -->
+    </div><!--/ card-heading -->
+    
+    <div class="card-item">
+    <img class="img-fluid" src="http://www.themashabrand.com/templates/bootsnipp/post/assets/img/1.jpg" alt="Image">
+    </div><!--/ card-item -->
+    <div class="card-base">
+    <ul class="float-right">
+    <li><a><i class="fa fa-comments"></i></a></li>
+    <li><a><em class="mr-5">12</em></a></li>
+    <li><a><i class="fa fa-share-alt"></i></a></li>
+    <li><a><em class="mr-3">03</em></a></li>
+    </ul>
+    <ul>
+    <li><a><i class="fa fa-thumbs-up"></i></a></li>
+    <li><a href="#"><img src="http://www.themashabrand.com/templates/bootsnipp/post/assets/img/users/3.jpeg" class="img-fluid rounded-circle" alt="User"></a></li>
+    <li><a href="#"><img src="http://www.themashabrand.com/templates/bootsnipp/post/assets/img/users/1.jpg" class="img-fluid rounded-circle" alt="User"></a></li>
+    <li><a href="#"><img src="http://www.themashabrand.com/templates/bootsnipp/post/assets/img/users/5.jpg" class="img-fluid rounded-circle" alt="User"></a></li>
+    <li><a href="#"><img src="http://www.themashabrand.com/templates/bootsnipp/post/assets/img/users/2.jpg" class="img-fluid rounded-circle" alt="User"></a></li>
+    <li><a><span>242 Likes</span></a></li>
+    </ul>			   
+    </div><!--/ card-base -->
+    <div class="card-comments">
+    <span class="comment-avatar float-left">
+    <a href=""><img class="rounded-circle" src="http://www.themashabrand.com/templates/bootsnipp/post/assets/img/users/6.jpg" alt="..."></a>                            
+    </span>
+    <div class="search">
+    <input placeholder="Write a comment" type="text">
+    <button><i class="fa fa-camera"></i></button>
+    </div><!--/. Search -->
+    </div><!--/ card-like -->			  
+        
+</div><!--/ card -->
+
+</div><!--/ col-lg-6 -->	
+<div class="col-lg-3">
+<div class="shadow-lg p-4 mb-2 bg-white author">
+    <a href="http://www.themashabrand.com/">Get more from themashabrand.com</a>
+    <p>Bootstrap 4.1.0</p>
 </div>
+</div><!--/ col-lg-3 -->
+
+</div><!--/ row -->
