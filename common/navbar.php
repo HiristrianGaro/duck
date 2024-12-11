be45e382516bd4d6afc069b0ed05342175b083a6
Christian Gallo <christiangallo20@gmail.com>
2024-12-11 19:45:43 +0100

    added button to add a post

--- common/navbar.php
+++ common/navbar.php
@@ -13,10 +13,12 @@
       <li><a href="#" class="nav-load nav-link text-body" data-target="#"><i class="bi bi-egg h4"></i></a></li>
       <li><a href="#" class="nav-link text-body" data-bs-toggle="collapse" data-bs-target="#SeachCollapse" aria-expanded="false" aria-controls="SeachCollapse" data-target="#"><i class="bi bi-search h4"></i></a></li>
       <li><a href="#" class="nav-load nav-link text-body" data-target="frontend/friends.php"><i class="bi bi-activity h4"></i></a></li>
-      <li><a href="#" class="nav-load nav-link text-body" id="user-profile" data-target="frontend/profilePage.html" data-username="<?php echo  $_SESSION["Username"];?>" ><i class="bi bi-feather h4"></i></a></li>
+      <li><a href="#" class="nav-load nav-link text-body" data-target="frontend/profilepage.php" data-username="<?php echo  $_SESSION["Username"];?>" ><i class="bi bi-feather h4"></i></a></li>
+
     </ul>
 
-    <div class="col-md-3 text-end">
+    <div class="col-3 text-end">
+      <a href="#" data-target="common/addPost.php" class="nav-load"><button type="button" class="justify-content-left btn btn-primary btn-round mr-md-1"><i class="bi bi-plus-square h4"></i></button></a>
       <a href="<?= SITE_DOMAIN?>/backend/logout.php"><button type="button" class="btn btn-primary btn-round mr-md-1">Logout</button></a>
     </div>
 
