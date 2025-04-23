5d43ec399881b5bcf59777aaa7c92a017d7f2722
Christian Gallo <christiangallo20@gmail.com>
2025-04-23 15:34:19 +0200

    Edit Profile to finish

--- frontend/profilepage.php
+++ frontend/profilepage.php
@@ -3,21 +3,26 @@
 
     if(isset($_SESSION['Status'])) { ?>
 <div class="mx-4">
-    <div class="profile-container card mb-4">
-        <div class="card-body d-flex align-items-center shadow-lg" id="profile-header">
-            
-        </div>
-        <div class="card-footer text-end">
-            <button class="btn btn-block float-right" id="follow-btn" data-action="{{Action}}" onclick="followAction(event)" ></button>
-        </div>
-    </div>
-    <div class="card mx-lg-5 mb-4 shadow">
-        <div class="card-body m-3">
-            <div class="row no-gutters" id="grid-item">
+    <div class="m-md-6">
+        <h1 class="text-center mt-4">Profile</h1>
+        <div class="profile-container card mb-4">
+            <div class="card-body d-flex align-items-center shadow-lg" id="profile-header">
                 
+            </div>
+            <div class="card-footer text-end">
+                <button class="btn btn-block float-right" id="follow-btn" data-action="{{Action}}" onclick="followAction(event)" ></button>
             </div>
         </div>
     </div>
+        <div>
+            <div class="card mx-lg-5 mb-4 shadow">
+                <div class="card-body m-3">
+                    <div class="row no-gutters" id="grid-item">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
 <?php } else { include("../frontend/items/UnauthorizedAllert.html");} ?>
